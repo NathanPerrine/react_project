6290ff513d2572e53c3e965288345a431af8a484
NathanPerrine <nathan.perrine@gmail.com>
2023-07-14 01:21:45 -0400

    moved tooltip anchor, testimonials started, updated testimonial field name

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -65,10 +65,10 @@ const Skills = () => {
                     whileInView={{ opacity: [0, 1] }}
                     transition={{ duration: 0.5 }}
                     className='app__skills-exp-work app__flex'
-                    data-tooltip-id={ work.name }
+                    // data-tooltip-id={ work.name }
                     key={work.name}
                     >
-                      <h4 className='bold-text'>{ work.name }</h4>
+                      <h4 className='bold-text' data-tooltip-id={ work.name }>{ work.name }</h4>
                       <p className='p-text'>{ work.company }</p>
                     </motion.div>
                     <Tooltip
@@ -76,7 +76,7 @@ const Skills = () => {
                       effect='solid'
                       className='skills-tooltip'
                       classNameArrow='skills-tooltip-arrow'
-                      place='bottom-start'
+                      place='top'
                     >
                       { work.desc }
                     </Tooltip>
