0e0a266062c5e91e6c4547db6e5c6acf0479adac
NathanPerrine <nathan.perrine@gmail.com>
2023-07-17 15:31:04 -0400

    testimonial styling / fixes

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
-import { HiChevronLeft, HiChevronRight } from 'react-icons'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi'
 
 import { AppWrap, MotionWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
@@ -11,6 +11,10 @@ const Testimonial = () => {
   const [brands, setBrands] = useState([])
   const [testimonials, setTestimonials] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
+
+  const handleClick = (index) => {
+    setCurrentIndex(index)
+  }
 
   useEffect(() => {
     const query = '*[_type == "testimonials"]'
@@ -43,8 +47,30 @@ const Testimonial = () => {
               </div>
             </div>
           </div>
+
+          <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? testimonials.length -1 : currentIndex - 1)}>
+              <HiChevronLeft />
+            </div>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+              <HiChevronRight />
+            </div>
+          </div>
+
         </>
       ) }
+
+      <div className='app__testimonial-brands app__flex'>
+        {brands.map((brand) => (
+          <motion.div
+            whileInView={{ opacity: [0, 1] }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            key={brand._id}
+          >
+            <img src={urlFor(brand.imgUrl)} alt={brand.name} />
+          </motion.div>
+        ))}
+      </div>
     </>
   )
 }
